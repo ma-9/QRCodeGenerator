c5fd6d4e6fadff5bc8c5a57df1ffbb02ddf5fbac
Manav Oza <manavoza99@gmail.com>
2019-11-20 00:39:12 +0530

    QRcode Design Complete

--- src/QRCODE/QRCODE.js
+++ src/QRCODE/QRCODE.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import QRCode from "react-qr-code";
-import AppBar from '../UI/AppBar/AppBar';
+import AppBar from "../UI/AppBar/AppBar";
 // import GenerateButton from "../UI/Generate_Button/circular";
+import "./QRCODE.css";
 
 class QRCODE extends Component {
   state = {
@@ -15,9 +16,11 @@ class QRCODE extends Component {
 
   render() {
     return (
-      <div>
-      <AppBar />
+      <div className="MAINAPP">
+        <AppBar />
+        <div className="myQRCODE">
         <QRCode className="qrCoDe" value={this.state.val} />
+        </div>
         <br />
         <input onChange={this.handleQrCode} />
       </div>
